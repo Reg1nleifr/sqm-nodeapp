630a020ac45b2c9ab03d64da60338cc6b3b1871d
Florian Reiter <flow.reiter@gmail.com>
2016-12-16 19:59:30 +0100

    jenkins to world

--- test/test.js
+++ test/test.js
@@ -3,8 +3,8 @@ var app = require('../app.js');
 
 describe('GET /', function() {
 
-  it('respond with hello jenkins', function(done) {
-    request(app).get('/').expect('hello jenkins', done);
+  it('respond with hello world', function(done) {
+    request(app).get('/').expect('hello world', done);
   });
 
   it('respond with asdf', function(done) {
