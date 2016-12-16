84a1af57ad76048354168907b9af5041284bc21c
Florian Reiter <flow.reiter@gmail.com>
2016-12-16 19:58:25 +0100

    First change

--- test/test.js
+++ test/test.js
@@ -3,15 +3,15 @@ var app = require('../app.js');
 
 describe('GET /', function() {
 
-  it('respond with hello world', function(done) {
-    request(app).get('/').expect('hello world', done);
+  it('respond with hello jenkins', function(done) {
+    request(app).get('/').expect('hello jenkins', done);
   });
 
-  it('respond with hello world', function(done) {
+  it('respond with asdf', function(done) {
     request(app).get('/asdf').expect('asdf', done);
   });
 
-  it('respond with hello world', function(done) {
+  it('respond with yxcv', function(done) {
     request(app).get('/yxcv').expect('yxcv', done);
   });
 
