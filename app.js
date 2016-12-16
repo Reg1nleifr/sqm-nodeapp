8922610d8ba24b62127e25574913981ac1541f3b
Florian Reiter <flow.reiter@gmail.com>
2016-12-16 22:46:47 +0100

    holla!

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('jenkins world');
+  res.send('hello world');
 });
 app.get('/asdf', function (req, res) {
   res.send('asdf');
