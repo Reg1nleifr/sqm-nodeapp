0185bef7591c4d06cf25410b951b03bf39cd569b
Florian Reiter <flow.reiter@gmail.com>
2016-12-19 23:19:20 +0100

    blubb

--- test/test.js
+++ test/test.js
@@ -4,7 +4,7 @@ var app = require('../app.js');
 describe('GET /', function() {
 
   it('respond with hello jenkins', function(done) {
-    request(app).get('/').expect('hello jenkins', done);
+    request(app).get('/').expect('hello jenkins!', done);
   });
 
   it('respond with asdf', function(done) {
